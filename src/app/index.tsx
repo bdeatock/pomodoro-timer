5d5a6afce82bcde16b8a1ad8baf74fb79aa05f57
Ben Eatock <bdeatock@gmail.com>
2025-03-28 16:56:02 +0000

    Basic scaffolding of ui

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,24 @@
+import { Icon } from "@iconify/react";
+import MainTimer from "../components/main-timer";
+import ModeSwitcher from "../components/mode-switcher";
+
 const App = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-gray-900">
-      <h1 className="text-4xl font-bold text-gray-100">Pomodoro Timer</h1>
-    </div>
+    <>
+      <main className="flex h-screen flex-col items-center justify-center gap-4 bg-gray-900 text-gray-100">
+        <h1 className="absolute top-0 mt-4 text-4xl font-bold text-gray-100">
+          Pomodoro Timer
+        </h1>
+
+        <ModeSwitcher />
+        <p>Time until long break: 48m</p>
+        <MainTimer />
+        <p>Today: 2h 15m</p>
+        <button type="button">
+          <Icon icon="ic:round-settings" className="h-6 w-6 text-gray-300" />
+        </button>
+      </main>
+    </>
   );
 };
 
---
+++ src/components/main-timer.tsx
@@ -0,0 +1,9 @@
+const MainTimer = () => {
+  return (
+    <div className="flex h-40 w-40 items-center justify-center rounded-full bg-gray-800">
+      <p>25:00</p>
+    </div>
+  );
+};
+
+export default MainTimer;
