b5aadc3d56bbbae13311c4e0cf969f82f1f85f0d
Ben Eatock <bdeatock@gmail.com>
2025-03-28 16:29:55 +0000

    Move app to new directory and update styling

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <>
-      <h1 className="text-4xl font-bold">Pomodoro Timer</h1>
-    </>
-  );
-};
-
-export default App;
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-gray-100">Pomodoro Timer</h1>
+    </div>
+  );
+};
+
+export default App;
