c1174efb105b88688d8ea4b5bba98da4b5bb30b3
Ben Eatock <bdeatock@gmail.com>
2025-03-29 13:15:19 +0000

    Install shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   base: "/pomodoro-timer/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
