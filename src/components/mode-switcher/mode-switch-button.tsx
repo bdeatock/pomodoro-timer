f938ce83741b3ec4e0ff672148ceeace420d5de1
Ben Eatock <bdeatock@gmail.com>
2025-04-01 08:17:21 +0100

    Add cursor pointer to button components

--- src/components/mode-switcher/mode-switch-button.tsx
+++ src/components/mode-switcher/mode-switch-button.tsx
@@ -16,7 +16,7 @@ const ModeSwitchButton: React.FC<ModeSwitchButtonProps> = ({
     <button
       type="button"
       onClick={() => setMode(buttonMode)}
-      className={`${buttonMode === mode ? colour : "bg-foreground hover:text-gray-400"} my-2 w-32 rounded-lg transition-colors duration-300`}
+      className={`${buttonMode === mode ? colour : "bg-foreground hover:text-gray-400"} my-2 w-32 cursor-pointer rounded-lg transition-colors duration-300`}
     >
       {buttonMode}
     </button>
